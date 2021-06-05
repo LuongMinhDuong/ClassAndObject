0e2ab5639a61c81b0c9ea38ece35d206f2c7a858
luongminhduong <Duong40541@gmail.com>
2021-06-05 10:09:49 +0700

    first commit

---
+++ src/advance/dev/Student.java
@@ -0,0 +1,17 @@
+package advance.dev;
+
+public class Student {
+	public String name;
+	public int age;
+	public String address;
+	public int number;
+	public float markAverage;
+	public Student(String name, int age, String address, int number, float markAverage) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.address = address;
+		this.number = number;
+		this.markAverage = markAverage;
+	}
+}
