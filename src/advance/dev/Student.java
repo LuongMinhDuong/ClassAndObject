e7e43d960969906bd45d25b69c713f88db6d794d
LuongMinhDuong <82139952+LuongMinhDuong@users.noreply.github.com>
2021-06-05 10:58:50 +0700

    Update Student.java

--- src/advance/dev/Student.java
+++ src/advance/dev/Student.java
@@ -4,7 +4,7 @@ public class Student {
 	public String name;
 	public int age;
 	public String address;
-	public int number;
+	public String number;
 	public float markAverage;
 	public Student(String name, int age, String address, String number, float markAverage) {
 		super();
